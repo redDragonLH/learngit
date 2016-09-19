e353196f486c83f26462defa53b48fd109cd8fe8
刘鹤 <1332983774@qq.com>
2016-09-19 13:56:56 +0800

    add gulp-sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,12 @@
 var gulp = require('gulp');
-
-// gulp-load-plugins
-/*var gulp = require('gulp');
-var gulpLoadPlugins = require('gulp-load-plugins');
-var plugins = gulpLoadPlugins();*/
-// 上三行等于下一行
-var $ = require('gulp-load-plugins')();
-
-// gulp-task-listing
-var taskListing = require('gulp-task-listing');
-
-var argv = require('yargs').argv;
-
-//
-var gutil = require('gulp-util');
-
-var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('sass', function () {
   return gulp.src('./sass/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'));
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(gulp.dest('./dist'));
 });
 
-
-// gulp.task('default', ['help']);
-// gulp.task('help', $.taskListing);
-
-/*gulp.task('default', function() {
-  gulp.task('help', $.taskListing);
-});*/
+gulp.task('default', function () {
+  gulp.watch('./sass/**/*.scss', ['sass']);
+});
