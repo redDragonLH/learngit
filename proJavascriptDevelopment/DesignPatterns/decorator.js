bec374be6e77cbabc796abac889eb475d46d7edc
六合 <1332983774@qq.com>
2017-02-11 23:06:08 +0800

    使用装饰模式

--- proJavascriptDevelopment/DesignPatterns/decorator.js
+++ proJavascriptDevelopment/DesignPatterns/decorator.js
@@ -58,3 +58,32 @@ AutoCompleteFieldDecorator.prototype.createElement = function () {
 
 // 使用
 // 创建一个空的<form> 标签，一个代表<input type = 'search'> 表单域的新 FormField 对象
+var form = document.createElement("form"),
+    formField = new FormField("search", "Enter your search term");
+
+// 使用装饰者为所生成的表单域元素添加 maxLength 和 autocomplete 属性，实现对 formField 对象的嫖客
+formField = new MaxLengthFieldDecorator(formField, 255);
+formField = new AutoCompleteFieldDecorator(formField, "off");
+
+// 创建该HTML 表单域元素并将其添加至 <form> 元素
+form.appendChild(formField.createElement());
+
+// 添加一个事件处理函数至该 <form> 标签的 submit 事件， 防止在我们添加的这个表单域当中没有值的情况下提交表单
+form.addEventListener("submit", function(e){
+  // 屏蔽表单的默认提交动作
+  e.preventDefault();
+
+  // 检测该表单域是否通过检验，例如是否包含值
+  if(formField.isValid()) {
+    // 如果是，继续运行并提交
+    form.submit();
+  } else {
+    // 提示错误
+    alert("没有值");
+  }
+}, false);
+
+// 页面加载完成，添加该表单至当前页面
+window.addEventListener("load", function(){
+  document.body.appendChild(form);
+}, false);
