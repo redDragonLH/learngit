da4e0522147f6f2704f7a629dd598beea6f20a64
六合 <1332983774@qq.com>
2017-06-30 13:50:36 +0800

    .

--- algorithm/enumerate/frog.js
+++ algorithm/enumerate/frog.js
@@ -1,10 +1,11 @@
 // 青蛙问题
 // 外部地址：http://blog.csdn.net/nnnnnnnnnnnny/article/details/51584284
 ;(function(){
-  var frog = {
+  var frog = function(){
     this.start=function(){
+        
+    };
 
-    }
   };
 
 })();
