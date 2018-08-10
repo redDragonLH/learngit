8701b7b98c71b558e5ab88d2934addfb527e15d5
刘鹤 <liuhe@csdn.net>
2018-08-10 10:35:43 +0800

    逻辑完成，有bug

--- dataStructure/BinaryTree.js
+++ dataStructure/BinaryTree.js
@@ -1,3 +1,4 @@
+;(function(){
 class Node{
   constructor( key ){
     this.key = key,
@@ -16,30 +17,149 @@ const insertNode = function(node, newNode){
     if(node.right === null){
       node.right = newNode
     } else { // 搜索下一层
-      insertNode(node.right, newNode)
+      insertNode( node.right, newNode )
     }
   }
+}
+// 中序遍历 左-根-右
+const  inOrderTraverseNode = function( node ){
+  if( node !== null ){
+    inOrderTraverseNode(node.left)
+    console.log(node.key);
+    inOrderTraverseNode(node.right)
+  }
+}
+// 先序遍历 根-左-右
+const preOrderTraverse = function( node ){
+  if( node !== null ){
+    console.log(node.key)
+    preOrderTraverseNode(node.left)
+    preOrderTraverseNode(node.right)
+  }
+}
+// 后序遍历 左-右-根
+const postOrderTraverseNode = function( node ){
+  if( node !== null ){
+    postOrderTraverseNode( node.left )
+    postOrderTraverseNode( node.right )
+    console.log( node.key )
+}
+// 查找最小值
+const minNode = function( node ){
+  if( node ){
+    while (node && node.left !== null ) {
+      node = node.left
+    }
+    return node.key
+  }
+  return null
+}
+// 查找最大值
+const maxNode = function( node ){
+  if( node ) {
+    while ( node && node.right !== null ){
+      node = node.right
+    }
+    return node.key
+  }
+  return null
+}
+// 搜索
+const searchNode = function( node, key ){
+  if( node === null ) {
+    return false
+  } 
+  if ( key < node.key ){
+    return searchNode( node.left, key )
+  } else if ( key > node.key ) {
+    return searchNode( node.right, key )
+  } else {
+    return true
+  }
+}
+// 移除节点
+const removeNode = function( node, key){
+  if( node == null) {
+    return null
+  }
+  if( key < node.key ){
+    node.left = removeNode( node.left, key)
+    return node
+  } else if( key > node.key ){
+    node.right = removeNode( node.right, key )
+    return node
+  } else {
+    // 需要移除的节点时一个叶子节点
+    if( node.left === null && node.right === null ) {
+      node = null
+      return node
+    }
+    // 需要移除的节点包含一个子节点
+    if ( node.left === null ) {
+      node = node.right 
+      return node
+    } else if ( node.right === null ) {
+      node = node.left
+      return node
+    }
+    // 需要移除的节点包含两个子节点
+    let aux = findMinNode( node.right )
+    node.key = aux.key
+    node.right = removeNode( node.right, axu.key )
+    return node
+  }
+}
+const findMinNode = function( node ) {
+  if (node ){
+    while (node && node.left !== null ) {
+      node = node.left
+    }
+    return node
+  }
+  return null
 }
 class BinaryTreeNode {
   constructor(){
     this.root = null;
   }
-  insert(key){
-    let newNode = new Node(key)
-    if(this.root === null){ // 根节点
+  insert( key ){
+    let newNode = new Node( key )
+    if( this.root === null ){ // 根节点
       this.root = newNode;
-    }else{
-      insertNode(this.root,newNode);
+    } else {
+      insertNode( this.root, newNode );
     }
   }
-  getroot(){
-    return this.root;
+  inOrderTraverse(){
+    inOrderTraverseNode( this.root )
+  }
+  preOrderTraverse (){
+    preOrderTraverseNode( this.root )
+  }
+  postOrderTraverse (){
+    postOrderTraverseNode( this.root )  
+  }
+  findMin (){
+    return minNode( this.root )
+  }
+  findMax (){
+    return maxNode( this.root )
+  }
+  search (key) {
+    return searchNode( this.root, key ) 
+  }
+  remove (key) {
+    removeNode( this.root, key)
   }
 }
 let binaryTree = new BinaryTreeNode();
-binaryTree.insert(10);
-console.log(binaryTree.getroot());
-binaryTree.insert(19);
-console.log(binaryTree.getroot());
-binaryTree.insert(9);
-console.log(binaryTree.getroot());
+
+var arr = [9,6,3,8,12,15]
+
+// arr.map(item => {
+//        binaryTree.insert(item) 
+//   });
+binaryTree.inOrderTraverse()
+binaryTree.preOrderTraverse()
+binaryTree.postOrderTraverse()
+})();
