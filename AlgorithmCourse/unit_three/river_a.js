fd920de8dd087810df14eb71cecc7ab66ef107e7
刘鹤 <liuhe@csdn.net>
2018-12-25 10:28:56 +0800

    数据结构

--- AlgorithmCourse/unit_three/river_a.js
+++ AlgorithmCourse/unit_three/river_a.js
@@ -1,3 +1,36 @@
 /**
  * 狼、羊、菜和农夫过河问题 js 实现部分
  */
+
+ const Action = {
+        'GO_SELF' : 'GO_SELF',
+        'GO_WITH_WOLF': 'GO_WITH_WOLF',
+        'GO_WITH_SHEEP': 'GO_WITH_SHEEP',
+        'GO_WITH_VEGETABLE': 'GO_WITH_VEGETABLE',
+        'BACK_SELF': 'BACK_SELF',
+        'BACK_WITH_WOLF': 'BACK_WITH_WOLF',
+        'BACK_WITH_SHEEP': 'BACK_WITH_SHEEP',
+        'BACK_WITH_VEGETABLE': 'BACK_WITH_VEGETABLE'
+}
+function ProcessFarmerGo(){}
+function ProcessFarmerGoTakeWolf(){}
+function ProcessFarmerGoTakeSheep(){}
+function ProcessFarmerGoTakeVegetable(){}
+function ProcessFarmerBack(){}
+function ProcessFarmerBackTakeWolf(){}
+function ProcessFarmerBackTakeSheep(){}
+function ProcessFarmerBackTakeVegetable(){}
+console.log(Action.GO_SELF);
+const actMap = [
+    {Action.GO_SELF: ProcessFarmerGo},
+    {Action.GO_WITH_WOLF: ProcessFarmerGoTakeWolf },
+    {Action.GO_WITH_SHEEP: ProcessFarmerGoTakeSheep},
+    {Action.GO_WITH_VEGETABLE: ProcessFarmerGoTakeVegetable},
+    {Action.BACK_SELF: ProcessFarmerBack},
+    {Action.BACK_WITH_WOLF: ProcessFarmerBackTakeWolf},
+    {Action.BACK_WITH_SHEEP: ProcessFarmerBackTakeSheep},
+    {Action.BACK_WITH_VEGETABLE:ProcessFarmerBackTakeVegetable}
+]
+
+
+
