047b22ed8410c242a346d11fca0b472a46d225cb
刘鹤 <liuhe@csdn.net>
2019-01-17 17:48:26 +0800

    过河部分

--- AlgorithmCourse/unit_three/river_a.js
+++ AlgorithmCourse/unit_three/river_a.js
@@ -1,18 +1,40 @@
 /**
  * 狼、羊、菜和农夫过河问题 js 实现部分
  */
+ LOCATION = {
+   LEFT: 'left',
+   RIGHT: 'right',
+ }
+class ItemState {
+  constructor() {
+    this.farmer    = LOCATION.LEFT;
+    this.wolf      = LOCATION.LEFT;
+    this.sheep     = LOCATION.LEFT;
+    this.vegetable = LOCATION.LEFT;
 
- const Action = {
-        'GO_SELF' : 'GO_SELF',
-        'GO_WITH_WOLF': 'GO_WITH_WOLF',
-        'GO_WITH_SHEEP': 'GO_WITH_SHEEP',
-        'GO_WITH_VEGETABLE': 'GO_WITH_VEGETABLE',
-        'BACK_SELF': 'BACK_SELF',
-        'BACK_WITH_WOLF': 'BACK_WITH_WOLF',
-        'BACK_WITH_SHEEP': 'BACK_WITH_SHEEP',
-        'BACK_WITH_VEGETABLE': 'BACK_WITH_VEGETABLE'
+    this.curAction;
+  }
+  IsSameState(state){
+    return (farmer == state.farmer) && (wolf == state.wolf) && (sheep == state.sheep) && (vegetable == state.vegetable);
+  }
+  PrintStates(){
+    
+  }
+  IsFinalState(){
+    return ( (farmer == LOCATION.RIGHT) && (wolf == LOCATION.RIGHT) && (sheep == LOCATION.RIGHT) && (vegetable == LOCATION.RIGHT) );
+  }
 }
-function ProcessFarmerGo(){}
+
+function ProcessFarmerGo(current, next){
+  if(current.farmer !=== LOCATION.LEFT){
+    return false;
+  }
+  next = current;
+  next.farmer = LOCATION.RIGHT;
+  next.curAction = Action.GO_SELF;
+  
+  return IsCurrentStateValid(next)
+}
 function ProcessFarmerGoTakeWolf(){}
 function ProcessFarmerGoTakeSheep(){}
 function ProcessFarmerGoTakeVegetable(){}
@@ -20,17 +42,42 @@ function ProcessFarmerBack(){}
 function ProcessFarmerBackTakeWolf(){}
 function ProcessFarmerBackTakeSheep(){}
 function ProcessFarmerBackTakeVegetable(){}
-console.log(Action.GO_SELF);
+ let Action = {
+        GO_SELF : ProcessFarmerGo,
+        GO_WITH_WOLF: ProcessFarmerGoTakeWolf,
+        GO_WITH_SHEEP: ProcessFarmerGoTakeSheep,
+        GO_WITH_VEGETABLE: ProcessFarmerGoTakeVegetable,
+        BACK_SELF: ProcessFarmerBack,
+        BACK_WITH_WOLF: ProcessFarmerBackTakeWolf,
+        BACK_WITH_SHEEP: ProcessFarmerBackTakeSheep,
+        BACK_WITH_VEGETABLE: ProcessFarmerBackTakeVegetable,
+}
 const actMap = [
-    {Action.GO_SELF: ProcessFarmerGo},
-    {Action.GO_WITH_WOLF: ProcessFarmerGoTakeWolf },
-    {Action.GO_WITH_SHEEP: ProcessFarmerGoTakeSheep},
-    {Action.GO_WITH_VEGETABLE: ProcessFarmerGoTakeVegetable},
-    {Action.BACK_SELF: ProcessFarmerBack},
-    {Action.BACK_WITH_WOLF: ProcessFarmerBackTakeWolf},
-    {Action.BACK_WITH_SHEEP: ProcessFarmerBackTakeSheep},
-    {Action.BACK_WITH_VEGETABLE:ProcessFarmerBackTakeVegetable}
+    {GO_SELF: ProcessFarmerGo},
+    {GO_WITH_WOLF: ProcessFarmerGoTakeWolf },
+    {GO_WITH_SHEEP: ProcessFarmerGoTakeSheep},
+    {GO_WITH_VEGETABLE: ProcessFarmerGoTakeVegetable},
+    {BACK_SELF: ProcessFarmerBack},
+    {BACK_WITH_WOLF: ProcessFarmerBackTakeWolf},
+    {BACK_WITH_SHEEP: ProcessFarmerBackTakeSheep},
+    {BACK_WITH_VEGETABLE:ProcessFarmerBackTakeVegetable}
 ]
+function IsProcessedState(states, newstates){
+  let it
+  return it != states.end();
+}
+function IsCurrentStateValid(current){
+  if(current.wolf === current.sheep && current.sheep !== current.farmer){
+    return false;
+  }
+  if( current.vegetable === current.sheep && current.sheep != current.farmer){
+    return false;
+  }
+  return true;
+}
 
 
+function SearchStates( states ){
+  
+}
 
