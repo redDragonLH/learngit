75365ab6ad79073415df7af99b7aac28a4076bae
刘鹤 <liuhe@csdn.net>
2019-01-29 10:00:54 +0800

    https://github.com/inte2000/play_with_algo/blob/master/albert/albert.cpp 思考题，没啥思路，暂时搁置

---
+++ AlgorithmCourse/unit_three/albert.js
@@ -0,0 +1,39 @@
+/**
+ * 爱因斯坦思考题
+ */
+ const GROUPS_ITEMS = 5;
+ const GROUPS_COUNT = 5;
+ 
+ const COLOR_BLUE    = 0;
+ const COLOR_RED     = 1;
+ const COLOR_GREEN   = 2;
+ const COLOR_YELLOW  = 3;
+ const COLOR_WHITE   = 4;
+ 
+ 
+ const NATION_NORWAY     = 0;
+ const NATION_DANMARK    = 1;
+ const NATION_SWEDEND    = 2;
+ const NATION_ENGLAND    = 3;
+ const NATION_GERMANY    = 4;
+ 
+ 
+ const DRINK_TEA     = 0;
+ const DRINK_WATER   = 1;
+ const DRINK_COFFEE  = 2;
+ const DRINK_BEER    = 3;
+ const DRINK_MILK    = 4;
+ 
+ const PET_HORSE   = 0;
+ const PET_CAT     = 1;
+ const PET_BIRD    = 2;
+ const PET_FISH    = 3;
+ const PET_DOG     = 4;
+ 
+ const CIGARET_BLENDS      = 0;
+ const CIGARET_DUNHILL     = 1;
+ const CIGARET_PRINCE      = 2;
+ const CIGARET_PALLMALL    = 3;
+ const CIGARET_BLUEMASTER  = 4;
+ 
+ 
