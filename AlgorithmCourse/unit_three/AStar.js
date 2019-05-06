d4826dc5fcd118c30abce70345d63a9f646ef414
刘鹤 <liuhe@csdn.net>
2019-05-06 14:21:07 +0800

    upload images and a start

--- AlgorithmCourse/unit_three/AStar.js
+++ AlgorithmCourse/unit_three/AStar.js
@@ -53,16 +53,32 @@ class Astar {
     this.map = map; // 地图展开为二维数组
     this.openList = []; //
     this.closeList = []; //
-    this.star = star;
-    this.end = end;
+    this.star = getPoint(star, this.map);
+    this.end = getPoint(end, this.map);
     this.H = computeH(this.star, this.end);
     this.G = {
       0: 10,
       1: 14,
     };
-    this.point = {
-      pos: [],
-      parent: {},
-    };
+    this.point = buildJustNowPoint( this.star);
+  }
+  buildJustNowPoint(nowpoint, low = {}){
+    let point = {
+      pos: nowpoint,
+      parent: low
+    }
+    if(low !== {}) {
+      point.F = getF(point, low);
+    }
+    return point;
+  }
+  getF(point,parentP){
+    return this.H + this.G[isG(point,parentP)];
+  }
+  isG(point,parentP){
+    if(!!Math.abs(parentP.pos[0]- point.pos[0]) && !!Math.abs(parentP.pos[1]- point.pos[1])){
+      return 1;
+    }
+    return 0;
   }
 }
