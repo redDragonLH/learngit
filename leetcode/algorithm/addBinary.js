2772c315f979cd5220f3c716384c7d1cf7178c96
刘鹤 <he.liu@aswatson>
2020-03-25 13:43:00 +0800

    67.二进制求和-位运算尝试

--- leetcode/algorithm/addBinary.js
+++ leetcode/algorithm/addBinary.js
@@ -50,6 +50,23 @@ var addBinary = function(a, b) {
 
  /**
   * 位操作
-  * （先学习位操作）
+  * 
+  * 两位非零数异或得到的结果是两数相减的绝对值
+  * 
+  * 使用异或可以获取到没有进位的两数的和
+  * 
+  * 使用按位与然后向左移一位可以获取两数之和的进位
+  * 
+  * 相加可得最后结果
+  */
+
+ var addBinaryByBit = function(a, b) {
+   let num1 = BigInt(parseInt(a,2));
+   let num2 = BigInt(parseInt(b,2));
+   return ((num1 ^ num2) + ((num1 & num2) << 1)).toString(2);
+ }
+ console.log(addBinaryByBit('11','1'));
+ /**
+  * 超大数据有问题
   */
 // https://leetcode-cn.com/problems/add-binary/solution/er-jin-zhi-qiu-he-by-leetcode/
