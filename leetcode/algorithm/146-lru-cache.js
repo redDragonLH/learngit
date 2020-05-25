61cbb01640cd6f123cd916e41c9fa714aea9954f
liuhe <ok1332983774@163.com>
2020-05-25 18:19:34 +0800

    j ava 题解

--- leetcode/algorithm/146-lru-cache.js
+++ leetcode/algorithm/146-lru-cache.js
@@ -8,7 +8,8 @@
 
  /**
  * @param {number} capacity
- * 不能用时间，存取的时候调用时间都是一样的～～
+ * 不能用时间，存取的时候调用时间都是一样的～～好几个核心同时调用么，深井冰啊～
+ * 
  */
 var LRUCache = function(capacity) {
     this.capacity = capacity;
@@ -75,3 +76,86 @@ LRUCache.prototype.delete = function() {
 var obj = new LRUCache(10)
 var param_1 = obj.get(1)
 obj.put(2,1)
+
+/**
+ * hash表+双向链表法
+ * 
+ * java代码
+ */
+
+/**
+ * public calss LRUCache {
+ *      class DLinkedNode {
+ *          int key;
+ *          int value;
+ *          DlinkedNode prev;
+ *          DLinkedNode next;
+ *          public DLinkedNode() {}
+ *          public DLinkedNode(int _key,int _value) {key = _key; value = _value;}
+ *      }
+ *      private Map<Integer,DLinkedNode> cache = new HashMap<Intger,DLinkedNode>();
+ *      private int size;
+ *      private int capacity;
+ *      private DLinkedNode head, tail;
+ * 
+ *      public LRUCache(int capacity) {
+ *          this.size = 0;
+ *          this.capacity = capacity;
+ *          // 使用伪头部和伪尾部节点
+ *          head = new DLinkedNode();
+ *          tail = new DLinkedNode();
+ *          head.next = tail;
+ *          tail.prev = head;
+ *      }
+ *      public int get(int key) {
+ *          DLinkedNode node = cache.get(key);
+ *          if(node == null) {
+ *              return -1;
+ *          }
+ *          moveToHead(node);
+ *          return node.value;
+ *      }
+ *      public void put(int key,int value) {
+ *          DLinkedNode node = cache.get(key);
+ *          if(node === null) {
+ *              // 如果 key 不存在，创建新的节点
+ *              DLinkedNode newNode = new DLinkedNode(key,value)
+ *              // 添加进哈希表
+ *              cache.put(key,newNode);
+ *              // 添加至双向链表头部
+ *              addToHead(newNode);
+ *              ++size;
+ *              if(size > capacity) {
+ *                  //如果超出容量，删除双向链表的尾部节点
+ *                  DLinkedNode tail = removeTail();
+ *                  // 删除哈希表中对应的项
+ *                  cache.remove(tail.key);
+ *                  --size;
+ *              }
+ *          }else {
+ *              // 如果 key 存在，先通过哈希表定位，再修改value,并移到头部
+ *              node.value = value;
+ *              moveToHead(node);
+ *          }
+ *      }
+ *      private void addToHead(DLinkedNode node) {
+ *          node.prev = head;
+ *          node.next = head.next;
+ *          head.next.prev = node;
+ *          head.next = node;
+ *      }
+ *      private void removeNode(DLinkedNode node) {
+ *          node.prev.next = node.next;
+ *          node.next.prev = node.prev;
+ *      }
+ *      private void moveToHead(DLinkedNode node) {
+ *          removeNode(Node);
+ *          addToHead(node);
+ *      }
+ *      private DLinkedNode removeTail() {
+ *          DLinkdeNode res = tail.prev;
+ *          removeNode(res);
+ *          return res;
+ *      }
+ * }
+ */
