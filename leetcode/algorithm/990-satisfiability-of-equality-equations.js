bc3ecaa470ea891dbc804f4ceb7145af71cfdcad
liuhe <ok1332983774@163.com>
2020-06-08 18:10:21 +0800

    990 思路以及java代码 完成

--- leetcode/algorithm/990-satisfiability-of-equality-equations.js
+++ leetcode/algorithm/990-satisfiability-of-equality-equations.js
@@ -10,27 +10,48 @@
  *      1. 先找到转换的方法，应该可以赋不同的值
  *          1. 长度只有4，
  *          2. 方式只有两种 == ，!= 
- * 
- * @param {string[]} equations
- * @return {boolean}
+ * 思路2： 并查集
+ *      1. 因为相等具有传递性，例： a == b, b == c,那么  a == c，这样的话，在有相同符号的不同等式里，这些等式是相关联的，只要数组内的不等式中不同时包含相关联的等式的符号，即算可以通过判定
  */
-var equationsPossible = function(equations) {
-    let equation = '';
-    equations.forEach(e => {
-        if(e[1]+e[2] === '=='){
-            equation.indexOf(e[0]) === -1 && (equation+=e[0])
-            equation.indexOf(e[3]) === -1 && (equation+=e[3])
-        }
-    })
-    for (let i = 0; i < equations.length; i++) {
-        let e= equations[i];
-        if(e[1]+e[2] === '!='){
-            if(e[0] === e[3] || (equation.indexOf(e[0])>-1 && equation.indexOf(e[3])>-1)) return false;
-        }
-        
-    }
-    return true;
-};
 
 
-console.log(equationsPossible(["c==c","b==d","x!=z"]))
+/**
+ * java 代码
+ * 
+ * class Solution {
+ *      public booean equationsPossible(String[] equations) {
+ *          int length = equations.length;
+ *          int[] parent = now int[26];
+ *          for(int i = 0; i < 26; i++) {
+ *              parent[i] = i;
+ *          }
+ *          for(String str : equations) {
+ *              if(str.charAt(1) == '=') {
+ *                  int index1 = str.charAt(0) - 'a';
+ *                  int index2 = str.charAt(3) - 'a';
+ *                  union(parent,index1,index,2);
+ *              }
+ *          }
+ *          for (String str : equations) {
+ *              if(str.charAt(1) == '!') {
+ *                  int index1 = str.charAt(0) - 'a';
+ *                  int index2 = str.charAt(3) - 'a';
+ *                  if(find(parent,index1)==find(parent, index2)) {
+ *                      return false;
+ *                  }
+ *              }
+ *          }
+ *          return true;
+ *      }
+ *      public void union(int[] parent,int index1,int index2) {
+ *          parent[find(parent,index1)] = find(parent,index2);
+ *      }
+ *      public int find(int[] parent,int index){
+ *          while(parent[index] != index) {
+ *              parent[index] = parent[parent[index]];
+ *              index = parent[index];
+ *          }
+ *          return index;
+ *      }
+ * }
+ */
