95eec37a5d3eddc93f552a2dbaf15734f9f581d9
liuhe <ok1332983774@163.com>
2020-06-16 18:00:26 +0800

    297 第三方题解记录

--- leetcode/algorithm/297-serialize-and-deserialize-binary-tree.js
+++ leetcode/algorithm/297-serialize-and-deserialize-binary-tree.js
@@ -78,3 +78,73 @@ var preOrder = (root,arr) => {
 console.log(deserialize('[1,2,3,null,null,4,5]'));
 
 console.log(deserialize(serialize(deserialize('[1,2,3,null,null,4,5]'))));
+
+function TreeNode(val,i) {
+    this.val = val;
+    this.index = i
+    this.left = this.right = null;
+ }
+
+/**
+ * ---------------------------------------------------------------------------------------------------
+ * 
+ * 第三方题解 BFS （广度优先搜索）
+ * 
+ * https://leetcode-cn.com/problems/serialize-and-deserialize-binary-tree/solution/shou-hui-tu-jie-gei-chu-dfshe-bfsliang-chong-jie-f/
+ */
+/**
+ * Encodes a tree to a single string.
+ *
+ * @param {TreeNode} root
+ * @return {string}
+ */
+const serialize = (root) => {
+    const queue = [root]
+    let res = []
+    while (queue.length) {
+      const node = queue.shift()
+      if (node) { // 出列的节点 带出子节点入列
+        res.push(node.val)
+        queue.push(node.left) // 不管是不是null节点都入列
+        queue.push(node.right)
+      } else {
+        res.push('X')
+      }
+    }
+    return res.join(',')
+  }
+  
+/**
+ * 左子节点 2 * i
+ * 右子节点 2 * i + 1
+ */
+/**
+ * Decodes your encoded data to tree.
+ *
+ * @param {string} data
+ * @return {TreeNode}
+ */
+const deserialize = (data) => {
+    if (data == 'X') return null       // 就一个'X'，只有一个null
+    const list = data.split(',')       // 序列化字符串转成list数组
+    const root = new TreeNode(list[0]) //首项是根节点值，为它创建节点
+    const queue = [root] // 初始放入root，待会出列考察
+    let cursor = 1       // 从list第二项开始遍历
+    while (cursor < list.length) {      // 指针越界就退出
+      const node = queue.shift()        // 父节点出列考察
+      const leftVal = list[cursor]      // 获取左子节点值
+      const rightVal = list[cursor + 1] // 获取右子节点值
+      if (leftVal !== 'X') {   // 左子节点值是有效值
+        const leftNode = new TreeNode(leftVal) // 创建节点
+        node.left = leftNode   // 成为当前出列节点的左子节点
+        queue.push(leftNode)   // 它是未来的爸爸，入列等待考察
+      }
+      if (rightVal !== 'X') {  // 右子节点值是有效值
+        const rightNode = new TreeNode(rightVal) // 创建节点
+        node.right = rightNode // 成为当前出列节点的右子节点
+        queue.push(rightNode)  // 它是未来的爸爸，入列等待考察
+      }
+      cursor += 2              // 指针前进2位
+    }
+    return root // 返回根节点
+  }
