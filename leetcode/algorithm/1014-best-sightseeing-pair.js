d7643850f7bbf1e37104018478a98a73149c2385
liuhe <ok1332983774@163.com>
2020-06-17 10:17:13 +0800

    1014 最佳观光组合 计算元素前最大值版-进行简单初始化以及重构

--- leetcode/algorithm/1014-best-sightseeing-pair.js
+++ leetcode/algorithm/1014-best-sightseeing-pair.js
@@ -13,23 +13,25 @@
  *  步骤1 计算最大值：就是当前元素+i 每次比对就好
  * 
  * 思路2 的优化还是不够，虽然速度提升巨大但是还在最差速度徘徊
+ * 
+ * 最优代码的基本思路和我一样，那么为啥速率差了那么多～～继续进行简单重构
  * @param {number[]} A
  * @return {number}
  */
 var maxScoreSightseeingPair = function(A) {
-    let maxb = 0;
+    let len = A.length;
+    let maxb = A[0]; // 初始化左侧最大值
     let max = 0;
-    for (let i = 1; i < A.length; i++) {
-        let bef = i-1
-        maxb = Math.max(maxb,A[bef]+bef);
-        max = Math.max(max,maxb + A[i]-i)
+    for (let i = 1; i < len; i++) {
+        max = Math.max(max,maxb + A[i]-i); //因为不是从0 开始计算所以可以先进行最高分的计算（不先计算也不行，要不然可能会出现i+i的情况）
+        maxb = Math.max(maxb,A[i]+i); // 然后获取当前元素及之前重的最大值
     }
     return max
 };
 console.log(maxScoreSightseeingPair([8,1,5,2,6]));
 /**
- * 计算元素最大值的方案
+ * 计算元素最大值的方案（简单重构后）
  * 
- * 执行用时 :120 ms, 在所有 JavaScript 提交中击败了26.97%的用户
- * 内存消耗 :40.6 MB, 在所有 JavaScript 提交中击败了100.00%的用户
+ * 执行用时 :88 ms, 在所有 JavaScript 提交中击败了43.82%的用户
+ * 内存消耗 :40.5 MB, 在所有 JavaScript 提交中击败了100.00%的用户
  */
