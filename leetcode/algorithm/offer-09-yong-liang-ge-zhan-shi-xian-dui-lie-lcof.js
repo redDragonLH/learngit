176310f1c00e76792129a15e55ba6fc53061d0bd
liuhe <ok1332983774@163.com>
2020-06-30 11:10:23 +0800

    剑指offer 09 用两个栈实现队列，修改逻辑

--- leetcode/algorithm/offer-09-yong-liang-ge-zhan-shi-xian-dui-lie-lcof.js
+++ leetcode/algorithm/offer-09-yong-liang-ge-zhan-shi-xian-dui-lie-lcof.js
@@ -28,21 +28,20 @@ CQueue.prototype.appendTail = function(value) {
  * @return {number}
  */
 CQueue.prototype.deleteHead = function() {
-    if(!this.tail.length)return -1;
-    let r = 0;
-    while(true) {
-        let e = this.tail.pop();
-        if(!this.tail.length){
-            r = e;
-            break;
-        }else {
-            this.head.push(e);
+    
+    if (!this.head.length) {
+        // 把栈一的数据都倒入栈二，反向
+        while (this.tail.length) {
+            this.head.push(this.tail.pop());
         }
+    } 
+    // 还空就完蛋了
+    if (!this.head.length) {
+        return -1;
+    } else {
+        // 栈二里面是栈一的倒叙，所以pop出来的是栈一的头
+        return this.head.pop();
     }
-    while(this.head.length){
-        this.tail.push(this.head.pop());
-    }
-    return r;
 };
 
 /**
@@ -58,7 +57,7 @@ console.log(obj.deleteHead());
 var param_2 = obj.deleteHead()
 
 /**
- * 执行用时： 572 ms , 在所有 JavaScript 提交中击败了45.91%的用户
+ * 执行用时： 480 ms , 在所有 JavaScript 提交中击败了83.71%的用户
  * 内存消耗：48 MB, 在所有 JavaScript 提交中击败了100.00%的用户
  */
 
@@ -66,35 +65,41 @@ var param_2 = obj.deleteHead()
  * java
  * 
 class CQueue {
-        Stack head;
-        Stack tail;
+    // Deque表示双端队列。双端队列是在两端都可以进行插入和删除的队列。Deque是一个比Stack和Queue功能更强大的接口，它同时实现了栈和队列的功能。ArrayDeque和LinkeList实现了Deque接口。
+    // 这里定义时用的是父类型，这样在初始化的时候就可以比较灵活，可能会用任意子类型
+    Deque<Integer> stack1; // <> 是泛型 在类型后边增加 <type> 就是内部储存的是这个类型的数据，可能是相关类型之一
+    Deque<Integer> stack2;
+    
     public CQueue() {
-        head = new Stack();
-        tail = new Stack();
+        // LinkedList ： 双向链表。它也可以被当作堆栈、队列或双端队列进行操作。
+        stack1 = new LinkedList<Integer>();// 使用了 LinkedList，，内部储存的泛型的数字数据
+        stack2 = new LinkedList<Integer>();
     }
     
     public void appendTail(int value) {
-        tail.push(value);
+        stack1.push(value);
     }
     
     public int deleteHead() {
-        if(tail.isEmpty()) return -1;
-        int ret = -1;
-        while(true) {
-            int ele = (int)tail.pop();
-            if(tail.isEmpty()) {
-                ret = ele;
-                break;
-            } else {
-                head.push(ele);
+        // 方案因为不会有其他操作，所以在删除时用第二个栈保存第一个栈的倒序，这样删的时候就删第二个栈，第二个栈空了再从第一个栈导入
+        // 如果第二个栈为空
+        if (stack2.isEmpty()) {
+            // 把栈一的数据都倒入栈二，反向
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
             }
+        } 
+        // 还空就完蛋了
+        if (stack2.isEmpty()) {
+            return -1;
+        } else {
+            // 栈二里面是栈一的倒叙，所以pop出来的是栈一的头
+            int deleteItem = stack2.pop();
+            return deleteItem;
         }
-        while(!head.isEmpty()){
-            tail.push(head.pop());
-        }
-        return ret;
     }
 }
+
  */
 /**
  * 超级慢的java 代码
