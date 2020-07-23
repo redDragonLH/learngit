43418520121f00a25efd289f46bf07b62d7ea48e
liuhe <ok1332983774@163.com>
2020-07-23 10:14:52 +0800

    64 优化dp 创建，时间变长

--- leetcode/algorithm/64-minimum-path-sum.js
+++ leetcode/algorithm/64-minimum-path-sum.js
@@ -13,19 +13,20 @@
  * 3. 初始值 dp[0][0] = grid[0][0]
  * @param {number[][]} grid
  * @return {number}
+ * 可以由二维数组转为1维,因为只能向右向下,所以可以由一个一维数组承载数据然后新的覆盖久的
  */
 var minPathSum = function(grid) {
     let row = grid.length;
     if(!row) return row; 
     let line = grid[0].length;
-    let dp = new Array(row);
+    // 使用新的数组创建方法
+    const dp = Array.from(Array(row), () => Array(line)) // 时间还变长了~~
     
     for (let i = 0; i < row; i++) {
-        dp[i]=[];
-        dp[i][0] = grid[i][0] +(i>0&& dp[i-1][0])
+        dp[i][0] = grid[i][0] +(i > 0 && dp[i-1][0])
     }
     for (let i = 1; i < line; i++) {
-        dp[0][i] = grid[0][i] +(i>0&& dp[0][i-1])
+        dp[0][i] = grid[0][i] +(i > 0 && dp[0][i-1])
     }
     for (let i = 1; i < row; i++) {
         for (let j = 1; j < line; j++) {
@@ -42,6 +43,6 @@ console.log(minPathSum([
   ]));
 /**
  * 二维数组版,还可以用以为一维数组保存数据
- * 执行用时：92 ms, 在所有 JavaScript 提交中击败了21.58%的用户
+ * 执行用时：96 ms, 在所有 JavaScript 提交中击败了16.88%的用户
  * 内存消耗：40 MB, 在所有 JavaScript 提交中击败了14.29%的用户
  */
