95caca0e94f2c32fd2539332e35d239cf057ae9d
刘鹤 <ok1332983774@163.com>
2020-08-02 10:08:11 +0800

    114 二叉树展开 官方题解 前驱节点

--- leetcode/algorithm/114-flatten-binary-tree-to-linked-list.js
+++ leetcode/algorithm/114-flatten-binary-tree-to-linked-list.js
@@ -41,3 +41,29 @@ const preorderTraversal  = (root,list)=> {
  * 执行用时：100 ms, 在所有 JavaScript 提交中击败了22.09%的用户
  * 内存消耗：40.1 MB, 在所有 JavaScript 提交中击败了53.85%的用户
  */
+
+/**
+  * 官方题解 ： 寻找前驱节点
+  * 
+  * 对于当前节点，如果其左子节点不为空，则在其左子树中找到最右边的节点，作为前驱节点，
+  * 将当前节点的右子节点赋给前驱节点的右子节点，然后将当前节点的左子节点赋给当前节点的右子节点，
+  * 并将当前节点的左子节点设为空。对当前节点处理结束后，继续处理链表中的下一个节点，直到所有节点都处理结束
+  * 
+  */
+
+var flatten = function (root) {
+    let curr = root;
+    while (curr !== null) {
+        if(curr.left !== null) { // 因为要把curr 的right 放到 left的最右侧节点，如果没有就不用放~
+            const next = curr.left;
+            let pardecessor = next;
+            while (pardecessor.right !== null) {
+                pardecessor = pardecessor.right;
+            }
+            pardecessor.right = curr.right;
+            curr.left = null;
+            curr.right = next;
+        }
+        curr = curr.right;
+    }
+}
