ce1d875a81aa2dd37d8dd5b65e99bfccfa1383aa
刘鹤 <ok1332983774@163.com>
2020-08-02 09:39:12 +0800

    114 二叉树展开为链表

---
+++ leetcode/algorithm/114-flatten-binary-tree-to-linked-list.js
@@ -0,0 +1,43 @@
+/**
+ * 114. 二叉树展开为链表
+ * 
+ * 给定一个二叉树，原地将它展开为一个单链表。
+ */
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * 使用前缀遍历，先把数据提取出来，然后循环放入二叉树内
+ * 
+ * 前缀遍历会从左到右，从上到下遍历所有二叉树节点，正好与当前问题顺序一样
+ * @param {TreeNode} root
+ * @return {void} Do not return anything, modify root in-place instead.
+ */
+var flatten = function(root) {
+    let list = [];
+    preorderTraversal(root,list)
+    let prev = root;
+    for (let i = 1; i < array.length; i++) {
+        prev.right = list[i];
+        prev.left = null;
+        prev = prev.right
+    }
+};
+const preorderTraversal  = (root,list)=> {
+    if(root) {
+        list.push(root);
+        preorderTraversal(root.left,list)
+        preorderTraversal(root.right,list)
+    }
+}
+
+/**
+ * 执行用时：100 ms, 在所有 JavaScript 提交中击败了22.09%的用户
+ * 内存消耗：40.1 MB, 在所有 JavaScript 提交中击败了53.85%的用户
+ */
