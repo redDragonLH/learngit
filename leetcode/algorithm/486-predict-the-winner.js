c76408d7bcc44d9fb6a8c90e84f0ff94ac72d48d
liuhe <ok1332983774@163.com>
2020-09-01 10:19:28 +0800

    486 预测赢家

---
+++ leetcode/algorithm/486-predict-the-winner.js
@@ -0,0 +1,39 @@
+/**
+ * 486. 预测赢家
+ *
+ * 给定一个表示分数的非负整数数组。 玩家 1 从数组任意一端拿取一个分数，随后玩家 2 继续从剩余数组任意一端拿取分数，
+ * 然后玩家 1 拿，…… 。每次一个玩家只能拿取一个分数，分数被拿取之后不再可取。直到没有剩余分数可取时游戏结束。最终获得分数总和最多的玩家获胜。
+ * 给定一个表示分数的数组，预测玩家1是否会成为赢家。你可以假设每个玩家的玩法都会使他的分数最大化。
+ *
+ * 提示：
+ * 1 <= 给定的数组长度 <= 20.
+ * 数组里所有分数都为非负数且不会大于 10000000 。
+ * 如果最终两个玩家的分数相等，那么玩家 1 仍为赢家。
+ */
+
+/**
+ * 动态规划
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var PredictTheWinner = function (nums) {
+
+};
+
+/**
+class Solution {
+    public boolean PredictTheWinner(int[] nums) {
+        if(nums.length %2==0) return true;
+        return total(nums, 0, nums.length - 1, 1) >= 0;
+    }
+
+    public int total(int[] nums, int start, int end, int turn) {
+        if (start == end) {
+            return nums[start] * turn;
+        }
+        int scoreStart = nums[start] * turn + total(nums, start + 1, end, -turn);
+        int scoreEnd = nums[end] * turn + total(nums, start, end - 1, -turn);
+        return Math.max(scoreStart * turn, scoreEnd * turn) * turn;
+    }
+}
+ */
