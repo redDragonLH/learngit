4990f22a9f9115944492cd11b5e5d06a60b2b9dc
liuhe <ok1332983774@163.com>
2020-09-27 17:38:14 +0800

    235 二叉搜索树的最近公共祖先

--- leetcode/algorithm/235-lowest-common-ancestor-of-a-binary-search-tree.js
+++ leetcode/algorithm/235-lowest-common-ancestor-of-a-binary-search-tree.js
@@ -1,6 +1,6 @@
 /**
  * 235. 二叉搜索树的最近公共祖先
- * 
+ *
  * 给定一个二叉搜索树, 找到该树中两个指定节点的最近公共祖先。
  * 百度百科中最近公共祖先的定义为：“对于有根树 T 的两个结点 p、q，最近公共祖先表示为一个结点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大（一个节点也可以是它自己的祖先）。”
  */
@@ -22,16 +22,21 @@
  * @param {TreeNode} q
  * @return {TreeNode}
  */
-var lowestCommonAncestor = function(root, p, q) {
-    if((root.val == p || root.val === q ) || (root.val > p || root.val < q ) || (root.val < p || root.val > q)) {
-        return root.val;
-    }
-    if(root.val > p || root.val > q) {
-        lowestCommonAncestor(root.left,p, q)
-    }
-    if(root.val < p || root.val < q) {
-        lowestCommonAncestor(root.right,p, q)
-    }
+var lowestCommonAncestor = function (root, p, q) {
+  if (
+    root.val === p.val ||
+    root.val === q.val ||
+    (root.val > p.val && root.val < q.val) ||
+    (root.val < p.val && root.val > q.val)
+  ) {
+    return root;
+  }
+  if (root.val > p.val && root.val > q.val) {
+    return lowestCommonAncestor(root.left, p, q);
+  }
+  if (root.val < p.val && root.val < q.val) {
+    return lowestCommonAncestor(root.right, p, q);
+  }
 };
 /**
  * 执行用时：112 ms, 在所有 JavaScript 提交中击败了49%的用户
