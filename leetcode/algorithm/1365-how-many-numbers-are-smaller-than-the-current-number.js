c5cdab072d2603f81f83e7572f09587a5f075c0a
liuhe <ok1332983774@163.com>
2020-10-26 10:36:45 +0800

    1365 官方题解 二维数组保存数据排序

--- leetcode/algorithm/1365-how-many-numbers-are-smaller-than-the-current-number.js
+++ leetcode/algorithm/1365-how-many-numbers-are-smaller-than-the-current-number.js
@@ -31,3 +31,30 @@ var smallerNumbersThanCurrent = function(nums) {
  * 执行用时：104 ms, 在所有 JavaScript 提交中击败了77.29%的用户
  * 内存消耗：39.9 MB, 在所有 JavaScript 提交中击败了18.99%的用户
  */
+
+/**
+ * 官方题解
+ * 
+ * 使用二维数组保存排序后的顺序
+ * 
+ * 这样数组元素和位置就能很好的结合在一起
+ */
+var smallerNumbersThanCurrent = function(nums) {
+    const n = nums.length;
+    const data = new Array(n).fill(0).map(v => new Array(2).fill(0)); // 二维数组?
+    for (let i = 0; i < n; ++i) {
+        data[i][0] = nums[i]; // 数字
+        data[i][1] = i; // 位置
+    }
+    data.sort((a, b) => a[0] - b[0]); //排序
+
+    const ret = new Array(n); // 结果数组
+    let prev = -1; // ???
+    for (let i = 0; i < n; ++i) {
+        if (prev == -1 || data[i][0] !== data[i - 1][0]) { // 应该是考虑元素重复的问题
+            prev = i;
+        }
+        ret[data[i][1]] = prev;
+    }
+    return ret;
+};
