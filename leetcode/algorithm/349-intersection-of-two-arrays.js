fb5d7e712935340d95b48b124260e8b6e40bd399
liuhe <ok1332983774@163.com>
2020-11-02 10:14:53 +0800

    349 两个数组的交集 不进行结构转换版

--- leetcode/algorithm/349-intersection-of-two-arrays.js
+++ leetcode/algorithm/349-intersection-of-two-arrays.js
@@ -23,3 +23,24 @@ var intersection = function(nums1, nums2) {
  * 执行用时：96 ms, 在所有 JavaScript 提交中击败了32.78%的用户
  * 内存消耗：39.2 MB, 在所有 JavaScript 提交中击败了18.68%的用户
  */
+
+/**
+ * 不进行结构转换版
+ * 
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersection = function(nums1, nums2) {
+    const resule = [];
+    if(!nums1.length || !nums2.length) return resule;
+    nums1.forEach(a=> {
+        if(nums2.includes(a)&& resule.includes(a)) resule.push(a);
+    })
+    return resule;
+};
+/**
+ * 提高了8毫秒,还能再提么
+ * 执行用时：88 ms, 在所有 JavaScript 提交中击败了63.99%的用户
+ * 内存消耗：39 MB, 在所有 JavaScript 提交中击败了27.63%的用户
+ */
