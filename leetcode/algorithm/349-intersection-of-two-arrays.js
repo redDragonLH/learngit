5415c6ffe7af9f2b9fc416d70045cd6e765b6904
liuhe <ok1332983774@163.com>
2020-11-02 09:54:08 +0800

    349 两个数组的交集

---
+++ leetcode/algorithm/349-intersection-of-two-arrays.js
@@ -0,0 +1,25 @@
+/**
+ * 349. 两个数组的交集
+ * 
+ * 给定两个数组，编写一个函数来计算它们的交集。
+ */
+
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersection = function(nums1, nums2) {
+    const resule = [];
+
+    if(!nums1.length || !nums2.length) return resule;
+    let nums1 = new Set(...nums1);
+    let nums2 = new Set(...nums2);
+    nums1.forEach((a)=> {if(nums2.has(a))resule.push(a) })
+    return resule;
+};
+/**
+ * 应该没必要进行数据格式的转换,indexof也就够了
+ * 执行用时：96 ms, 在所有 JavaScript 提交中击败了32.78%的用户
+ * 内存消耗：39.2 MB, 在所有 JavaScript 提交中击败了18.68%的用户
+ */
