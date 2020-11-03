d54761b43c5d285eead4a12de2244ce8eb58c90d
liuhe <ok1332983774@163.com>
2020-11-03 14:10:52 +0800

    941 有效的山脉数组 双指针

--- leetcode/algorithm/941-valid-mountain-array.js
+++ leetcode/algorithm/941-valid-mountain-array.js
@@ -34,3 +34,32 @@ var validMountainArray = function(A) {
  * 执行用时：100 ms, 在所有 JavaScript 提交中击败了33.33%的用户
  * 内存消耗：40.6 MB, 在所有 JavaScript 提交中击败了31.54%的用户
  */
+
+
+/**
+ * 双指针想法,让两个指针往中间走
+ * @param {number[]} A
+ * @return {boolean}
+ */
+var validMountainArray = function(A) {
+    if(A.length <3) return false;
+    // if(A[0]>A[1]) return false;
+    let start =0,end = A.length-1;
+    let left = true,right = true;
+    while(left || right) {
+        if(A[start+1]>A[start])start++;
+        else left = false;
+        if(A[end-1]>A[end])end--;
+        else right=false
+    }
+    if(!start) return false;
+    if(end === A.length-1) return false;
+    return start === end;
+};
+/**
+ * 为啥还慢了~~~
+ * 行用时：116 ms, 在所有 JavaScript 提交中击败了9.42%的用户
+ * 内存消耗：40.9 MB, 在所有 JavaScript 提交中击败了14.62%的用户
+ * 
+ * 第二次运行就变成超过85%的人了
+ */
