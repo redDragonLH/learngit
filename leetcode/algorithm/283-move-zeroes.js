68975236ce49d5484f8a2e0b601f6d027fa2c583
liuhe <ok1332983774@163.com>
2020-11-20 09:01:02 +0800

    283 第三方题解

--- leetcode/algorithm/283-move-zeroes.js
+++ leetcode/algorithm/283-move-zeroes.js
@@ -34,3 +34,25 @@ console.log(moveZeroes([0,1,0,3,12]));
  * 执行用时：104 ms, 在所有 JavaScript 提交中击败了23.70%的用户
  * 内存消耗：40 MB, 在所有 JavaScript 提交中击败了5.01%的用户
  */
+
+/**
+ * 第三方最优题解
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+    // 双指针 
+    // index、i 两个指针，其中 index 为慢指针，记录非零值移动后的下标，最后将 index 后的所有元素置零
+    let index = 0
+    for (let i = 0; i < nums.length; i++ ) {
+        if (nums[i] !== 0) {
+            nums[index++] = nums[i]
+        }
+    }
+
+    // index 后的元素置零
+    while (index < nums.length) {
+        nums[index] = 0
+        index++
+    }
+};
