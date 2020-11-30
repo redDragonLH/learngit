ad0dff80875f52dc647d83693eb62edc3fb6697b
liuhe <ok1332983774@163.com>
2020-11-30 11:00:57 +0800

    767 官方题解

--- leetcode/algorithm/767-reorganize-string.js
+++ leetcode/algorithm/767-reorganize-string.js
@@ -57,3 +57,47 @@ var reorganizeString = function(S) {
 }
 reorganizeString("aaab")
 
+
+/**
+ * 
+ * 官方题解 贪心算法
+ * 
+ * 
+ * @param {*} c 
+ */
+const getIdx = (c) => c.charCodeAt() - 'a'.charCodeAt();
+const getAlpha = (c) => String.fromCharCode(c);
+var reorganizeString = function(S) {
+    if (S.length < 2) {
+        return S;
+    }
+    const counts = new Array(26).fill(0);
+    let maxCount = 0;
+    const length = S.length;
+    for (let i = 0; i < length; i++) {
+        const c = S.charAt(i);
+        counts[getIdx(c)]++;
+        maxCount = Math.max(maxCount, counts[getIdx(c)]);
+    }
+    if (maxCount > Math.floor((length + 1) / 2)) { // 一种字母超过二分之一就肯定会有相邻的情况
+        return "";
+    }
+
+    const reorganizeArray = new Array(length);
+    let evenIndex = 0, oddIndex = 1;
+    const halfLength = Math.floor(length / 2); // 半长
+    for (let i = 0; i < 26; i++) {
+        const c = getAlpha('a'.charCodeAt() + i);
+        while (counts[i] > 0 && counts[i] <= halfLength && oddIndex < length) {
+            reorganizeArray[oddIndex] = c;
+            counts[i]--;
+            oddIndex += 2;
+        }
+        while (counts[i] > 0) {
+            reorganizeArray[evenIndex] = c;
+            counts[i]--;
+            evenIndex += 2;
+        }
+    }
+    return reorganizeArray.join('');
+};
