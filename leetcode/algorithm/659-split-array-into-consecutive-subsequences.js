700e3b095911c08212ab845ef1fd9cc539967f04
liuhe <ok1332983774@163.com>
2020-12-04 10:55:08 +0800

    659 官方 贪心

--- leetcode/algorithm/659-split-array-into-consecutive-subsequences.js
+++ leetcode/algorithm/659-split-array-into-consecutive-subsequences.js
@@ -43,3 +43,57 @@ var isPossible = function(nums) {
  * 执行用时：724 ms, 在所有 JavaScript 提交中击败了14.00%的用户
  * 内存消耗：44.7 MB, 在所有 JavaScript 提交中击败了92.00%的用户
  */
+
+/**
+ * 官方题解  贪心算法
+ * 贪心算法: 在每一步选择中都采取在当前状态下最好或最优(即最有利)的选择,从而希望导致结果最好或最优.平常考虑问题中经常是这个思考模式,但是并无法察觉出来
+ * 
+ */
+var isPossible = function(nums) {
+    const countMap = new Map(); // 存储数组中的每个数字的剩余次数
+    const endMap = new Map(); // 存储数组中的每个数字作为结尾的子序列的数量。
+
+    // 初始化countMap 
+    for (const x of nums) {
+        const count = (countMap.get(x) || 0) + 1;
+        countMap.set(x, count);
+    }
+
+    for (const x of nums) {
+        // 怎么还会有countMap 没有的~~
+        const count = countMap.get(x) || 0;
+        if (count > 0) {
+            // 是否有以比这个小1的数字结尾的子数组
+            const prevEndCount = endMap.get(x - 1) || 0;
+            // 有则更新两个map
+            if (prevEndCount > 0) {
+                countMap.set(x, count - 1);
+                endMap.set(x - 1, prevEndCount - 1);
+                endMap.set(x, (endMap.get(x, 0) || 0) + 1);
+            } else {
+                // 直接取连着的两个,如果没有说明有一个子数组无法组成最小3个数量
+                const count1 = countMap.get(x + 1, 0);
+                const count2 = countMap.get(x + 2, 0);
+                if (count1 > 0 && count2 > 0) {
+                    countMap.set(x, count - 1);
+                    countMap.set(x + 1, count1 - 1);
+                    countMap.set(x + 2, count2 - 1);
+                    endMap.set(x + 2, (endMap.get(x + 2) || 0) + 1);
+                } else {
+                    return false;
+                }
+            }
+        }
+    }
+    return true;
+};
+/**
+ * 代码很简单,但是和贪心有什么关系呢
+ * 只考虑当前的步骤,而不考虑后面的吗
+ * 
+ * 1.贪心选择性质。所谓贪心选择性质是指所求问题的整体最优解可以通过一系列局部最优的选择，即贪心选择来达到。这是贪心算法可行的第一个基本要素，也是贪心算法与动态规划算法的主要区
+ * 
+ * 动态规划算法通常以自底向上的方式解各子问题，而贪心算法则通常以自顶向下的方式进行，以迭代的方式作出相继的贪心选择，每作一次贪心选择就将所求问题简化为规模更小的子问题。
+ * 对于一个具体问题，要确定它是否具有贪心选择性质，必须证明每一步所作的贪心选择最终导致问题的整体最优解。
+ * 2. 当一个问题的最优解包含其子问题的最优解时，称此问题具有最优子结构性质。问题的最优子结构性质是该问题可用动态规划算法或贪心算法求解的关键特征。
+ */
