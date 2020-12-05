2ec53b4cbe936d5dc8017a0ba5963520b11dcabc
LH <ok1332983774@163.com>
2020-12-05 14:58:23 +0800

    621 官方题解

--- leetcode/algorithm/621-task-scheduler.js
+++ leetcode/algorithm/621-task-scheduler.js
@@ -41,4 +41,23 @@ var leastInterval = function(tasks, n) {
     }
     return count
 };
+
+/**
+ * 官方题解 
+ */
+var leastInterval = function(tasks, n) {
+    const freq = _.countBy(tasks); // 转为对象，键值对为 字母和出现的次数
+
+    // 最多的执行次数
+    const maxExec = Math.max(...Object.values(freq));
+    // 具有最多执行次数的任务数量
+    let maxCount = 0;
+    Object.values(freq).forEach(v => {
+        if (v === maxExec) {
+            maxCount++;
+        }
+    })
+
+    return Math.max((maxExec - 1) * (n + 1) + maxCount, tasks.length);
+};
 leastInterval(["A","A","A","B","B","B"],2)
