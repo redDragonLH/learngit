e9fb43e083419573e90bacaa5ec7b9be72f71692
LH <ok1332983774@163.com>
2020-12-13 11:54:13 +0800

    217 暴力法

---
+++ leetcode/algorithm/217-contains-duplicate.js
@@ -0,0 +1,28 @@
+/**
+ * 217. 存在重复元素
+ * 
+ * 给定一个整数数组，判断是否存在重复元素。
+ * 如果任意一值在数组中出现至少两次，函数返回 true 。如果数组中每个元素都不相同，则返回 false 。
+ */
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    const len = nums.length;
+    if(len<2) return false;
+
+    const set = new Set();
+    for (let i = 0; i < len; i++) {
+        if(set.has(nums[i])) return true;
+        else set.add(nums[i])        
+    }
+    return false;
+};
+/**
+ * 暴力法
+ * 
+ * 执行用时：80 ms, 在所有 JavaScript 提交中击败了96.11%的用户
+ * 内存消耗：42 MB, 在所有 JavaScript 提交中击败了72.38%的用户
+ */
