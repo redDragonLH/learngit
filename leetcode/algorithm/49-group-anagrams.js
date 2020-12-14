0dcf0bba855886d59b34e19ff13ce574c384c152
liuhe <ok1332983774@163.com>
2020-12-14 11:07:10 +0800

    49 官方题解

--- leetcode/algorithm/49-group-anagrams.js
+++ leetcode/algorithm/49-group-anagrams.js
@@ -70,7 +70,7 @@ const isAnagrams = (str1,str2)=> {
 
 /**
  * 使用对象存储映射表
- * 哈希表方案
+ * 排序 +  哈希表方案
  */
 var groupAnagrams = function(strs) {
     const result = [[strs[0]]];
@@ -103,3 +103,41 @@ console.log(groupAnagrams(["",""]));
  * 执行用时：380 ms, 在所有 JavaScript 提交中击败了5.01%的用户
  * 内存消耗：46.8 MB, 在所有 JavaScript 提交中击败了92.41%的用户
  */
+
+/**
+ * 官方题解 排序
+ * 
+ * 简洁,效率
+ * 
+ * 排序简单了不少,最终结果是从中间层转的,这样控制一个就可以了,并且使用map,这样减少了一个循环
+ */
+var groupAnagrams = function(strs) {
+    const map = new Map();
+    for (let str of strs) {
+        let array = Array.from(str);
+        array.sort();
+        let key = array.toString();
+        let list = map.get(key) ? map.get(key) : new Array();
+        list.push(str);
+        map.set(key, list);
+    }
+    return Array.from(map.values());
+};
+
+/**
+ * 官方题解,计数
+ * 
+ * 由于互为字母异位词的两个字符串包含的字母相同，因此两个字符串中的相同字母出现的次数一定是相同的，故可以将每个字母出现的次数使用字符串表示，作为哈希表的键。
+ */
+var groupAnagrams = function(strs) {
+    const map = new Object();
+    for (let s of strs) {
+        const count = new Array(26).fill(0);
+        for (let c of s) {
+            count[c.charCodeAt() - 'a'.charCodeAt()]++;
+        }
+        // 数组就直接上,也是牛逼
+        map[count] ? map[count].push(s) : map[count] = [s];
+    }
+    return Object.values(map);
+};
