ebd6caf380826c2b6df57bdba24c9461a5370643
liuhe <ok1332983774@163.com>
2021-01-08 09:48:50 +0800

    189 旋转数组

--- leetcode/algorithm/189-rotate-array.js
+++ leetcode/algorithm/189-rotate-array.js
@@ -22,3 +22,29 @@ var rotate = function(nums, k) {
  * 执行用时：92 ms, 在所有 JavaScript 提交中击败了82.30%的用户
  * 内存消耗：39.7 MB, 在所有 JavaScript 提交中击败了33.53%的用户
  */
+
+/**
+ * 反转数组,昨天刚在编程珠玑上看到
+ */
+const reverse = (nums, start, end) => {
+    while (start < end) {
+        const temp = nums[start];
+        nums[start] = nums[end];
+        nums[end] = temp;
+        start += 1;
+        end -= 1;
+    }
+}
+
+var rotate = function(nums, k) {
+    k %= nums.length;
+    // 自建方法要比语言自带的方法快很多
+    reverse(nums, 0, nums.length - 1);
+    // nums.reverse()
+    reverse(nums, 0, k - 1);
+    reverse(nums, k, nums.length - 1);
+};
+/**
+ * 执行用时：84 ms, 在所有 JavaScript 提交中击败了96.22%的用户
+ * 内存消耗：38.5 MB, 在所有 JavaScript 提交中击败了91.54%的用户
+ */
