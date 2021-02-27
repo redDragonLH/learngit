461bffc820125183beb5d0e10bf91fb3ebd3030c
LH <ok1332983774@163.com>
2021-02-27 10:36:51 +0800

    395 至少有 k 个重复的最长子串 官方题解：滑动窗口

--- leetcode/algorithm/395-longest-substring-with-at-least-k-repeating-characters.js
+++ leetcode/algorithm/395-longest-substring-with-at-least-k-repeating-characters.js
@@ -74,3 +74,44 @@ const dfs = (s, l, r, k) => {
     }
     return ret;
 };
+
+/**
+ * 官方题解: 滑动窗口
+ */
+var longestSubstring = function(s, k) {
+    let ret = 0;
+    const n = s.length;
+    for (let t = 1; t <= 26; t++) {
+        let l = 0, r = 0;
+        const cnt = new Array(26).fill(0);
+        let tot = 0;
+        let less = 0;
+        while (r < n) {
+            cnt[s[r].charCodeAt() - 'a'.charCodeAt()]++;
+            if (cnt[s[r].charCodeAt() - 'a'.charCodeAt()] === 1) {
+                tot++;
+                less++;
+            }
+            if (cnt[s[r].charCodeAt() - 'a'.charCodeAt()] === k) {
+                less--;
+            }
+
+            while (tot > t) {
+                cnt[s[l].charCodeAt() - 'a'.charCodeAt()]--;
+                if (cnt[s[l].charCodeAt() - 'a'.charCodeAt()] === k - 1) {
+                    less++;
+                }
+                if (cnt[s[l].charCodeAt() - 'a'.charCodeAt()] === 0) {
+                    tot--;
+                    less--;
+                }
+                l++;
+            }
+            if (less == 0) {
+                ret = Math.max(ret, r - l + 1);
+            }
+            r++;
+        }
+    }
+    return ret;
+};
