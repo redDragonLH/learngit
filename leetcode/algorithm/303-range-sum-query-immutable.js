dc1b055fa997b34f54b4db644c1b6c26f187456f
liuhe <ok1332983774@163.com>
2021-03-01 09:18:31 +0800

    303 区域检索和-数组不可变 前缀和

--- leetcode/algorithm/303-range-sum-query-immutable.js
+++ leetcode/algorithm/303-range-sum-query-immutable.js
@@ -38,9 +38,46 @@ NumArray.prototype.sumRange = function(start, end) {
  * var param_1 = obj.sumRange(i,j)
  */
 
- /**
-  * 最直觉的解法,效率不高.如果是这样的话应该还有优化,更好的解法我估计是费劲了,应该就是缓存和数据吧
-  * 
-  * 执行用时：256 ms, 在所有 JavaScript 提交中击败了23.58%的用户
-  * 内存消耗：43.8 MB, 在所有 JavaScript 提交中击败了92.14%的用户
-  */
+/**
+ * 最直觉的解法,效率不高.如果是这样的话应该还有优化,更好的解法我估计是费劲了,应该就是缓存和数据吧
+ * 
+ * 执行用时：256 ms, 在所有 JavaScript 提交中击败了23.58%的用户
+ * 内存消耗：43.8 MB, 在所有 JavaScript 提交中击败了92.14%的用户
+ */
+
+
+/**
+ * 第三方优秀题解
+ */
+
+/**
+ * 前缀和
+ * @param {number[]} nums
+ */
+var NumArray = function (nums) {
+    const dp = []
+    // 存的时候就是前缀和,计算的时候用后面的减去前面的,就是中间的,厉害厉害
+    dp[0] = nums[0]
+    
+    for (let i = 1; i <= nums.length - 1; i++) {
+      dp[i] = dp[i - 1] + nums[i]
+    }
+    this.dp = dp
+  };
+  
+  /** 
+   * @param {number} i 
+   * @param {number} j
+   * @return {number}
+   */
+  NumArray.prototype.sumRange = function (i, j) {
+  
+    return this.dp[j] - (i == 0 ? 0 : this.dp[i - 1])
+  
+  };
+  
+  /**
+   * Your NumArray object will be instantiated and called as such:
+   * var obj = new NumArray(nums)
+   * var param_1 = obj.sumRange(i,j)
+   */
