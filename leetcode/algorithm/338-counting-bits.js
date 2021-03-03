745fbc583f81e48a1d0454d6980fc4cfba3ef8a8
liuhe <ok1332983774@163.com>
2021-03-03 10:44:24 +0800

    338 比特位计数 官方题解 第三方优秀题解

--- leetcode/algorithm/338-counting-bits.js
+++ leetcode/algorithm/338-counting-bits.js
@@ -55,3 +55,19 @@ var countBits = function(num) {
     }
     return bits;
 };
+
+/**
+ * 第三方优秀题解
+ * 
+ *  对于一个二进制数来说，如果它的最低位为1(%2 为 1）,则它与 n/2 的 1 个数相差1。 如果它的最低位为 0，则它与 n/2 的 1 个数相同 这样就可以用前面的推出后面的
+ * 
+public int[] countBits(int num) {
+    int dp[] = new int[num+1];
+    for (int i = 0; i <= num/2; i++) {
+        dp[i*2] = dp[i];
+        if(i*2+1 <= num)
+            dp[i*2+1] = dp[i] + 1;
+    }
+    return dp;
+}
+ */
