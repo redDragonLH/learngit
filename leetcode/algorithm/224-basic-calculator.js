a750b14c2bf9ad277f1dfa1f21fd18ab23bfcf5d
liuhe <ok1332983774@163.com>
2021-03-10 10:41:15 +0800

    224 官方题解

--- leetcode/algorithm/224-basic-calculator.js
+++ leetcode/algorithm/224-basic-calculator.js
@@ -67,3 +67,40 @@ var calculate = function (s) {
     }
     console.log(stock)
 }
+
+/**
+ * 官方题解: 括号展开 + 栈
+ */
+var calculate = function(s) {
+    const ops = [1];
+    let sign = 1;
+
+    let ret = 0;
+    const n = s.length;
+    let i = 0;
+    while (i < n) {
+        if (s[i] === ' ') {
+            i++;
+        } else if (s[i] === '+') {
+            sign = ops[ops.length - 1];
+            i++;
+        } else if (s[i] === '-') {
+            sign = -ops[ops.length - 1];
+            i++;
+        } else if (s[i] === '(') {
+            ops.push(sign);
+            i++;
+        } else if (s[i] === ')') {
+            ops.pop();
+            i++;
+        } else {
+            let num = 0;
+            while (i < n && !(isNaN(Number(s[i]))) && s[i] !== ' ') {
+                num = num * 10 + s[i].charCodeAt() - '0'.charCodeAt();
+                i++;
+            }
+            ret += sign * num;
+        }
+    }
+    return ret;
+};
