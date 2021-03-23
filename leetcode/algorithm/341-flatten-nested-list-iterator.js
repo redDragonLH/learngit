39e72df372cd31d8b03afb3613fb1678527eabb4
liuhe <ok1332983774@163.com>
2021-03-23 10:18:02 +0800

    341 扁平化嵌套列表迭代器 栈预处理，成功，使用已定义的接口

--- leetcode/algorithm/341-flatten-nested-list-iterator.js
+++ leetcode/algorithm/341-flatten-nested-list-iterator.js
@@ -1,6 +1,6 @@
 /**
  * 341. 扁平化嵌套列表迭代器
- * 
+ *
  * 给你一个嵌套的整型列表。请你设计一个迭代器，使其能够遍历这个整型列表中的所有整数。
  * 列表中的每一项或者为一个整数，或者是另一个列表。其中列表的元素也可能是整数或是其他列表。
  */
@@ -37,25 +37,24 @@
  * @param {NestedInteger[]} nestedList
  */
 var NestedIterator = function (nestedList) {
-    // 需要数组保存当前的位置,因为内部可能是嵌套的,所以用一个数无法描述嵌套数据
-    this.pos = [0];
+  // 需要数组保存当前的位置,因为内部可能是嵌套的,所以用一个数无法描述嵌套数据
+  this.pos = [0];
 
-    let zeroItem = nestedList[0]
-    while (typeof zeroItem === 'object') {
-        zeroItem = zeroItem[0];
-        this.pos.push(0)
-    }
-    this.pos.push(0)
-    this.nestedList = nestedList;
+  let zeroItem = nestedList[0];
+  while (typeof zeroItem === "object") {
+    zeroItem = zeroItem[0];
+    this.pos.push(0);
+  }
+  this.pos.push(0);
+  this.nestedList = nestedList;
 };
-
 
 /**
  * @this NestedIterator
  * @returns {boolean}
  */
 NestedIterator.prototype.hasNext = function () {
-    return !!this.pos.length
+  return !!this.pos.length;
 };
 
 /**
@@ -63,86 +62,120 @@ NestedIterator.prototype.hasNext = function () {
  * @returns {integer}
  */
 NestedIterator.prototype.next = function () {
-    let lastPos = 0;
-    let item = this.nestedList
-    this.pos.forEach(e => {
-        item = item[e]
-        lastPos = e
-    })
-    // this.pos[this.pos.length - 1] = lastPos++;
-    this.nextPos()
-    return item;
+  let lastPos = 0;
+  let item = this.nestedList;
+  this.pos.forEach((e) => {
+    item = item[e];
+    lastPos = e;
+  });
+  // this.pos[this.pos.length - 1] = lastPos++;
+  this.nextPos();
+  return item;
 };
 
 /**
  * 自己写的获取下个位置的逻辑太复杂了,而且实现也不是很好,
  */
 NestedIterator.prototype.nextPos = function () {
-    let item = this.nestedList
-    if(!this.pos.length) return false;
-    this.pos[this.pos.length - 1]++
+  let item = this.nestedList;
+  if (!this.pos.length) return false;
+  this.pos[this.pos.length - 1]++;
 
-    this.pos.forEach(e => {
-        item = item[e]
-    })
-    if (!item) {
-        this.pos.pop()
-        this.pos.length && this.pos[this.pos.length - 1]++
-    }
-    item = this.nestedList;
-    this.pos.forEach(e => {
-        item = item[e]
-    })
-    while(typeof item === 'object') {
-        item= item[0];
-        this.pos.push(0)
-    }
-    this.pos.push(0)
-}
+  this.pos.forEach((e) => {
+    item = item[e];
+  });
+  if (!item) {
+    this.pos.pop();
+    this.pos.length && this.pos[this.pos.length - 1]++;
+  }
+  item = this.nestedList;
+  this.pos.forEach((e) => {
+    item = item[e];
+  });
+  while (typeof item === "object") {
+    item = item[0];
+    this.pos.push(0);
+  }
+  this.pos.push(0);
+};
 /**
  * Your NestedIterator will be called like this:
  * var i = new NestedIterator(nestedList), a = [];
  * while (i.hasNext()) a.push(i.next());
-*/
-
+ */
 
 /**
  * 其实可以进行预处理,先扁平化,然后再用一个位置指针记录位置
- * 
- * 数据并不是嵌套数组,而是没有length 的数据,所以预处理有点坑
+ *
+ * 数据并不是嵌套数组,而是没有length 的数据,所以预处理有点坑,必须使用已经定义的接口
  * @constructor
  * @param {NestedInteger[]} nestedList
  */
-var NestedIterator = function(nestedList) {
+var NestedIterator = function (nestedList) {
     this.stack = [];
     this.pos = 0;
-    this.add(nestedList)
-};
-/**
- * @this NestedIterator
- * @returns {boolean}
- */
-NestedIterator.prototype.add = function(arr) {
-    for (let i = 0; i < arr.length; i++) {
-        if(typeof arr[i] === 'object'){
-            this.add(arr[i])
-        }
-        this.stack.push(arr[i])
+    for (let i = 0; i < nestedList.length; i++) {
+      if (nestedList[i].isInteger()) {
+        this.stack.push(nestedList[i].getInteger());
+      } else {
+        this.add(nestedList[i].getList());
+      }
     }
-};
+  };
+  /**
+   * @this NestedIterator
+   * @returns {boolean}
+   */
+  NestedIterator.prototype.add = function (arr) {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i].isInteger()) {
+        this.stack.push(arr[i].getInteger());
+      } else {
+        this.add(arr[i].getList());
+      }
+    }
+  };
+  
+  /**
+   * @this NestedIterator
+   * @returns {boolean}
+   */
+  NestedIterator.prototype.hasNext = function () {
+    return this.pos < this.stack.length;
+  };
+  
+  /**
+   * @this NestedIterator
+   * @returns {integer}
+   */
+  NestedIterator.prototype.next = function () {
+    return this.stack[this.pos++];
+  };
 
 /**
- * @this NestedIterator
- * @returns {boolean}
+ * 执行用时：124 ms, 在所有 JavaScript 提交中击败了37.44%的用户
+ * 内存消耗：48.7 MB, 在所有 JavaScript 提交中击败了33.65%的用户
  */
-NestedIterator.prototype.hasNext = function() {
-    return pos < this.stack.length;
-};
 
 /**
- * @this NestedIterator
- * @returns {integer}
+ * 官方题解
  */
-NestedIterator.prototype.next = function() {
-    return this.stack[pos++]
+var NestedIterator = function (nestedList) {
+  this.stack = nestedList;
+};
+
+NestedIterator.prototype.hasNext = function () {
+  while (this.stack.length !== 0) {
+    if (this.stack[0].isInteger()) {
+      return true;
+    } else {
+      let cur = this.stack[0].getList();
+      this.stack.shift();
+      this.stack.unshift(...cur);
+    }
+  }
+};
+
+NestedIterator.prototype.next = function () {
+  return this.stack.shift().getInteger();
 };
