5832f26dd67c35729a69654b4c03504b8c7ba5fe
liuhe <ok1332983774@163.com>
2021-03-29 09:48:37 +0800

    50 pow(x,n) 自题解 超出时间限制

---
+++ leetcode/algorithm/50-powx-n.js
@@ -0,0 +1,30 @@
+/**
+ * 50. Pow(x, n)
+ * 
+ * 实现 pow(x, n) ，即计算 x 的 n 次幂函数（即，xn）。
+ */
+
+/**
+ * 
+ * @param {number} x
+ * @param {number} n
+ * @return {number}
+ */
+var myPow = function (x, n) {
+    if (!n) return 1; // 所有数的0次方都为1
+    if (!x) return x;
+    let count = x;
+    let absn = Math.abs(n);
+
+    while (--absn) {
+        count = count * x
+    }
+    if (n < 0) count = 1 / count;
+    return count;
+};
+/**
+ * 2.00000
+ * -2147483648
+ * 
+ * 超出时间限制
+ */
