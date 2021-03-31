f7c021416dc0c54a04577a4e4d6605ec2c7d0a3f
liuhe <ok1332983774@163.com>
2021-03-31 09:56:08 +0800

    90 官方题解

--- leetcode/algorithm/90-subsets-ii.js
+++ leetcode/algorithm/90-subsets-ii.js
@@ -31,3 +31,32 @@ var subsetsWithDup = function (nums) {
   result.push([]);
   return result;
 };
+
+/**
+ * 官方题解
+ */
+var subsetsWithDup = function (nums) {
+  nums.sort((a, b) => a - b);
+  let t = [],
+    ans = [];
+  const n = nums.length;
+  // <<: 左移位,把1 的二进制数向左横移,移动的位置补零,2的n次方?
+  for (let mask = 0; mask < 1 << n; ++mask) {
+    t = [];
+    let flag = true;
+    for (let i = 0; i < n; ++i) {
+        // & 位运算: 按位与
+      if ((mask & (1 << i)) != 0) {
+        if (i > 0 && ((mask >> (i - 1)) & 1) == 0 && nums[i] == nums[i - 1]) {
+          flag = false;
+          break;
+        }
+        t.push(nums[i]);
+      }
+    }
+    if (flag) {
+      ans.push(t.slice());
+    }
+  }
+  return ans;
+};
