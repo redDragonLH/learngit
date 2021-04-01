136754e92d74937ce0febde510edd760e6a4603b
liuhe <ok1332983774@163.com>
2021-04-01 17:06:09 +0800

    1006 笨阶乘 官方题解

--- leetcode/algorithm/1006-clumsy-factorial.js
+++ leetcode/algorithm/1006-clumsy-factorial.js
@@ -63,3 +63,36 @@
  * 执行用时：116 ms, 在所有 JavaScript 提交中击败了12.50%的用户
  * 内存消耗：41.6 MB, 在所有 JavaScript 提交中击败了12.50%的用户
  */
+
+/**
+ * 官方题解 
+ * 
+ * 注意一点,减法时可以把数字置为这个数的负数,这样在最后处理的时候就可以直接加法,不需要处理符号和加减
+ */
+ var clumsy = function(N) {
+  const stack = [N--];
+
+  let index = 0; // 用于控制乘、除、加、减
+  while (N > 0) {
+      if (index % 4 == 0) {
+          stack.push(stack.pop() * N);
+      } else if (index % 4 == 1) {
+          const cur = stack.pop();
+          stack.push(cur > 0 ? Math.floor(cur / N) : Math.ceil(cur / N));
+      } else if (index % 4 == 2) {
+          stack.push(N);
+      } else {
+        // 减号注意
+          stack.push(-N);
+      }
+      index++;
+      N--;
+  }
+
+  // 把栈中所有的数字依次弹出求和
+  let sum = 0;
+  stack.forEach((element) => {
+      sum += element;
+  })
+  return sum;
+};
