2e5b51297437d51423370e1057306a9c098444c1
liuhe <ok1332983774@163.com>
2021-04-27 09:20:17 +0800

    938 二叉搜索树的范围和 递归版

---
+++ leetcode/algorithm/938-range-sum-of-bst.js
@@ -0,0 +1,38 @@
+/**
+ * 938. 二叉搜索树的范围和
+ *
+ * 给定二叉搜索树的根结点 root，返回值位于范围 [low, high] 之间的所有结点的值的和。
+ */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * 二叉搜索树是一种有序排列的二叉树,所以按照顺序轮询就可以,其实不用按顺序也挺好吧~~~
+ * @param {TreeNode} root
+ * @param {number} low
+ * @param {number} high
+ * @return {number}
+ */
+var rangeSumBST = function (root, low, high) {
+  const deep = (root) => {
+    let count = 0;
+    if (!root) {
+      return 0;
+    }
+    count += deep(root.left);
+    if (root.val > low && root.val < high) count += root.val;
+    count += deep(root.right);
+    return count;
+  };
+  return deep(root)
+};
+/**
+ * 递归要每次构建栈,增加操作
+ * 执行用时：260 ms, 在所有 JavaScript 提交中击败了46.14%的用户
+ * 内存消耗：64.8 MB, 在所有 JavaScript 提交中击败了23.64%的用户
+ */
