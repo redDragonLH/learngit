755bd55edecfbec4e1febe479cfe3405430c74d9
liuhe <ok1332983774@163.com>
2021-04-28 10:03:58 +0800

    633 平方数之和 双指针

--- leetcode/algorithm/633-sum-of-square-numbers.js
+++ leetcode/algorithm/633-sum-of-square-numbers.js
@@ -13,6 +13,7 @@
  * 可以用双指针解法,但是逻辑原理是什么,
  *
  * 失败,还是双循环的路子,first 走到头才处理last ,这个逻辑不对
+ * 双指针,不是快慢针
  * @param {number} c
  * @return {boolean}
  */
@@ -39,10 +40,29 @@ var judgeSquareSum = function (c) {
  */
 var judgeSquareSum = function (c) {
   for (let a = 0; a * a <= c; a++) {
-      // 一点点减过去
+    // 一点点减过去
     const b = Math.sqrt(c - a * a);
     if (b === parseInt(b)) {
       return true;
+    }
+  }
+  return false;
+};
+
+/**
+ * 双指针,不是快慢针
+ */
+var judgeSquareSum = function (c) {
+  let left = 0;
+  let right = Math.floor(Math.sqrt(c)); //  从两头开始处理
+  while (left <= right) {
+    const sum = left * left + right * right;
+    if (sum === c) {
+      return true;
+    } else if (sum > c) {
+      right--;
+    } else {
+      left++;
     }
   }
   return false;
