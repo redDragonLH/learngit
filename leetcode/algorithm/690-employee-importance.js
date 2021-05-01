777cda2b9495a2f0062551da9c54efcd7af77a44
LH <ok1332983774@163.com>
2021-05-01 10:54:33 +0800

    690 删除多余数据

--- leetcode/algorithm/690-employee-importance.js
+++ leetcode/algorithm/690-employee-importance.js
@@ -42,15 +42,6 @@
  * 
  */
 /**
- * Definition for Employee.
- * function Employee(id, importance, subordinates) {
- *     this.id = id;
- *     this.importance = importance;
- *     this.subordinates = subordinates;
- * }
- */
-
-/**
  * @param {Employee[]} employees
  * @param {number} id
  * @return {number}
