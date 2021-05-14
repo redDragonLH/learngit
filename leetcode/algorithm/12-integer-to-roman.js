3bcaee06cea823afbeaff13384e853f5a9061d9b
liuhe <ok1332983774@163.com>
2021-05-14 09:33:08 +0800

    12 整数转罗马数字 code style

--- leetcode/algorithm/12-integer-to-roman.js
+++ leetcode/algorithm/12-integer-to-roman.js
@@ -22,81 +22,95 @@
 
 /**
  * 官方题解 模拟
- * 
+ *
  * 制造已知信息映射表
  * @param {number} num
  * @return {string}
  */
- var intToRoman = function(num) {
-    const values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
-    const valueSymbols = new Map();
-    valueSymbols.set(1000, "M");
-    valueSymbols.set(900, "CM");
-    valueSymbols.set(500, "D");
-    valueSymbols.set(400, "CD");
-    valueSymbols.set(100, "C");
-    valueSymbols.set(90, "XC");
-    valueSymbols.set(50, "L");
-    valueSymbols.set(40, "XL");
-    valueSymbols.set(10, "X");
-    valueSymbols.set(9, "IX");
-    valueSymbols.set(5, "V");
-    valueSymbols.set(4, "IV");
-    valueSymbols.set(1, "I");
-    const roman = [];
-    for (const value of values) {
-        // 循环减去已知数据
-        const symbol = valueSymbols.get(value);
-        while (num >= value) {
-            num -= value;
-            roman.push(symbol);
-        }
-        if (num == 0) {
-            break;
-        }
+var intToRoman = function (num) {
+  const values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+  const valueSymbols = new Map();
+  valueSymbols.set(1000, "M");
+  valueSymbols.set(900, "CM");
+  valueSymbols.set(500, "D");
+  valueSymbols.set(400, "CD");
+  valueSymbols.set(100, "C");
+  valueSymbols.set(90, "XC");
+  valueSymbols.set(50, "L");
+  valueSymbols.set(40, "XL");
+  valueSymbols.set(10, "X");
+  valueSymbols.set(9, "IX");
+  valueSymbols.set(5, "V");
+  valueSymbols.set(4, "IV");
+  valueSymbols.set(1, "I");
+  const roman = [];
+  for (const value of values) {
+    // 循环减去已知数据
+    const symbol = valueSymbols.get(value);
+    while (num >= value) {
+      num -= value;
+      roman.push(symbol);
     }
-    return roman.join('');
+    if (num == 0) {
+      break;
+    }
+  }
+  return roman.join("");
 };
 
 /**
  * 官方题解 硬编码数字
- * @param {*} num 
- * @returns 
+ * @param {*} num
+ * @returns
  */
-var intToRoman = function(num) {
-    // 每个数都写出来
-    const thousands = ["", "M", "MM", "MMM"];
-    const hundreds = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"];
-    const tens     = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"];
-    const ones     = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
+var intToRoman = function (num) {
+  // 每个数都写出来
+  const thousands = ["", "M", "MM", "MMM"];
+  const hundreds = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"];
+  const tens = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"];
+  const ones = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
 
-    const roman = [];
-    roman.push(thousands[Math.floor(num / 1000)]);
-    roman.push(hundreds[Math.floor(num % 1000 / 100)]);
-    roman.push(tens[Math.floor(num % 100 / 10)]);
-    roman.push(ones[num % 10]);
-    return roman.join('');
+  const roman = [];
+  roman.push(thousands[Math.floor(num / 1000)]);
+  roman.push(hundreds[Math.floor((num % 1000) / 100)]);
+  roman.push(tens[Math.floor((num % 100) / 10)]);
+  roman.push(ones[num % 10]);
+  return roman.join("");
 };
 
 /**
  * 第三方优秀题解
- * 
+ *
  * 模拟的简明写法
  */
 /**
  * @param {number} num
  * @return {string}
  */
- var intToRoman = function(num) {
-    let str = ''
-    let luomaArr = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I']
-    let numberArr = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
-    // 双循环
-    for(let i = 0; num > 0 && i < 13; i++) {
-        while (numberArr[i] <= num) {
-            str += luomaArr[i]
-            num -= numberArr[i]
-        }
+var intToRoman = function (num) {
+  let str = "";
+  let luomaArr = [
+    "M",
+    "CM",
+    "D",
+    "CD",
+    "C",
+    "XC",
+    "L",
+    "XL",
+    "X",
+    "IX",
+    "V",
+    "IV",
+    "I",
+  ];
+  let numberArr = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+  // 双循环
+  for (let i = 0; num > 0 && i < 13; i++) {
+    while (numberArr[i] <= num) {
+      str += luomaArr[i];
+      num -= numberArr[i];
     }
-    return str
+  }
+  return str;
 };
