9de096034689f2bf1826cd169602eeaf825d8136
liuhe <ok1332983774@163.com>
2021-06-03 10:36:31 +0800

    525. 连续数组 官方题解

--- test.js
+++ test.js
@@ -1,14 +1,25 @@
 class A {
+  constructor(data){
+    console.log('a',data);
+    this.b=data;
+  }
     c() {
       return 2;
+    }
+    d(){
+      console.log('d',this.b);
+      return this.b
     }
   }
   
   class B extends A {
     constructor() {
-      super();
-      console.log(super.c); // 2
+      super(1);
+      console.log(super.c(),this.c);
+      console.log('b',super.b,this.b); 
+      console.log('d',super.d()); 
     }
   }
   
   let b = new B();
+  b.d()
