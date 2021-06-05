5d5ea6da1e14b2d10ff8c120f5d10fa25538d473
LH <ok1332983774@163.com>
2021-06-05 13:48:16 +0800

    203. 移除链表元素

---
+++ leetcode/algorithm/203-remove-linked-list-elements.js
@@ -0,0 +1,32 @@
+/**
+ * 203. 移除链表元素
+ * 
+ * 给你一个链表的头节点 head 和一个整数 val ，请你删除链表中所有满足 Node.val == val 的节点，并返回 新的头节点 。
+ */
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * 链表处理的一个注意点是头结点，
+ * @param {ListNode} head
+ * @param {number} val
+ * @return {ListNode}
+ */
+var removeElements = function (head, val) {
+    let link = { next: head }
+    let node = link
+    while (node) {
+        if (node.next && node.next.val === val) {
+            //  在删除next 节点之后 next 节点已经是个新节点了，所以这个新节点必须进入循环进行判断
+            node.next = node.next.next
+        } else {
+            node = node.next
+        }
+    }
+    return link.next
+};
