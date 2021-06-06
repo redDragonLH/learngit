5d0c0ffff6b85bd9429a5e456cda7f3eca2936e1
LH <ok1332983774@163.com>
2021-06-06 10:56:29 +0800

    474. 一和零 官方题解 动态规划

--- leetcode/algorithm/474-ones-and-zeroes.js
+++ leetcode/algorithm/474-ones-and-zeroes.js
@@ -70,3 +70,52 @@ var findMaxForm = function (strs, m, n) {
     }
 return count
 };
+
+/**
+ * 官方题解，动态规划
+ * 
+ * 此问题比背包问题多了一个维度，动态规划求解的第一步是确定问题的数据结构，确定方式应该是以要处理的物品/事情为主体，
+ * 以相关变量为副，像此问题，主体为字符串，那么数据结构的第一个元素应该是字符，然后是容量,最后结果: [str,0,1]转为dp数据结构为[str][0][1]
+ * 此三维数组dp[i][j][k] 表示在前i个字符串中，使用j个0和k个1的情况下最多可以得到的字符串数量，
+ * 
+ * 当字符串数量大于0时，对于strs中的第i个字符串，首先遍历该字符串得到其中的 0 和 1 的数量，分别记作zeros 和 ones，
+ * 然后对与 0 <= j <= m 和 0 <= k <= n,计算 dpp[i][j][k]
+ * 
+ * 动态规划的核心就是从头开始计算，然后当前元素与前一个元素进行对比判断，所以在动态规划中，初始值和转移方程才会时最重要的
+ * 
+ * 能理解动态规划的实现逻辑，但是对于状态转移方程还是不太理解
+ * @param {string[]} strs
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ */
+ var findMaxForm = function(strs, m, n) {
+    const length = strs.length;
+    const dp = new Array(length + 1).fill(0).map(() => new Array(m + 1).fill(0).map(() => new Array(n + 1).fill(0)));
+    for (let i = 1; i <= length; i++) {
+        // 获取当前字符串的0，1的总量
+        const zerosOnes = getZerosOnes(strs[i - 1]);
+        let zeros = zerosOnes[0], ones = zerosOnes[1];
+        for (let j = 0; j <= m; j++) {
+            for (let k = 0; k <= n; k++) {
+                // 不选当前字符串的情况，那么当前的dp值就等于上一个元素
+                dp[i][j][k] = dp[i - 1][j][k]; // j,k都是当前元素的相关数据，只需改变i就可以
+                if (j >= zeros && k >= ones) { // 如果0，1数量符合条件
+                    // 为啥j,k在位置上减数据，三维数据的数据结构还是不熟
+                    dp[i][j][k] = Math.max(dp[i][j][k], dp[i - 1][j - zeros][k - ones] + 1);
+                }
+            }
+        }
+    }
+    return dp[length][m][n];
+};
+
+const getZerosOnes = (str) => {
+    const zerosOnes = new Array(2).fill(0);
+    const length = str.length;
+    for (let i = 0; i < length; i++) {
+        zerosOnes[str[i].charCodeAt() - '0'.charCodeAt()]++;
+    }
+    return zerosOnes;
+
+}
