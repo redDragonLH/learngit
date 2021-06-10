cd7b1b46964eca1369ce3cf4b3e2af35072708d1
liuhe <ok1332983774@163.com>
2021-06-10 09:48:21 +0800

    518 零钱兑换 II 失败，缺少交叉对比

---
+++ leetcode/algorithm/518-coin-change-2.js
@@ -0,0 +1,26 @@
+/**
+ * 518. 零钱兑换 II
+ *
+ * 给定不同面额的硬币和一个总金额。写出函数来计算可以凑成总金额的硬币组合数。假设每一种面额的硬币有无限个。
+ */
+
+/**
+ * 动态规划:当前钱数下有多少种组合吗,但是coins 有多种,无法按照,从后一个数据推前一个吧
+ *
+ * @param {number} amount
+ * @param {number[]} coins
+ * @return {number}
+ */
+var change = function (amount, coins) {
+  let len = coins.length;
+  // 当前钱数下多少种组合么,可以吧coins 分开计算然后加在一起, (但是这样无法交叉计算了)
+  // 0 圆钱的时候所以元素都是零
+  let dp = new Array(len).fill(0);
+  for (let i = 1; i <= amount; i++) {
+    for (let j = 0; j < len; j++) {
+      // 应该是上一个数加上当前元素coins是不是等于当前的钱
+      dp[j] = dp[j] + coins[j] === i ? dp[j] + coins[j] : dp[j];
+    }
+  }
+};
+change(5, [1, 2, 5]);
