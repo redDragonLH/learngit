e59ec636b0dd3b4195fd8f84829b524f5e9f6bfa
liuhe <ok1332983774@163.com>
2021-06-11 11:08:20 +0800

    279. 完全平方数 官方题解： 四平方和定理

--- leetcode/algorithm/279-perfect-squares.js
+++ leetcode/algorithm/279-perfect-squares.js
@@ -51,3 +51,38 @@ var numSquares = function (n) {
   }
   return f[n];
 };
+
+/**
+ * 官方题解: 数学方法
+ * 
+ * 四平方和定理
+ */
+ var numSquares = function(n) {
+    if (isPerfectSquare(n)) {
+        return 1;
+    }
+    if (checkAnswer4(n)) {
+        return 4;
+    }
+    for (let i = 1; i * i <= n; i++) {
+        let j = n - i * i;
+        if (isPerfectSquare(j)) {
+            return 2;
+        }
+    }
+    return 3;
+}
+
+// 判断是否为完全平方数
+const isPerfectSquare = (x) => {
+    const y = Math.floor(Math.sqrt(x));
+    return y * y == x;
+}
+
+// 判断是否能表示为 4^k*(8m+7)
+const checkAnswer4 = (x) => {
+    while (x % 4 == 0) {
+        x /= 4;
+    }
+    return x % 8 == 7;
+}
