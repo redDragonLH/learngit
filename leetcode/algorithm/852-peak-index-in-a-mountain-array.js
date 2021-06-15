17fd9a3025b19d8b2cb079ca6e71e81864ebef10
liuhe <ok1332983774@163.com>
2021-06-15 10:16:04 +0800

    852. 山脉数组的峰顶索引 官方题解

--- leetcode/algorithm/852-peak-index-in-a-mountain-array.js
+++ leetcode/algorithm/852-peak-index-in-a-mountain-array.js
@@ -16,7 +16,7 @@
  */
 var peakIndexInMountainArray = function (arr) {
   //当 中点小于左点(low 点),,说明峰顶在中点的左边,
-  //当 中点大于左点,可能在中点的右边,也可能是在中点的左边
+  //当 中点大于左点,峰顶可能在中点的右边,也可能是在中点的左边
 };
 
 
@@ -33,3 +33,24 @@ var peakIndexInMountainArray = function (arr) {
  * 执行用时：76 ms, 在所有 JavaScript 提交中击败了93.87%的用户
  * 内存消耗：39.6 MB, 在所有 JavaScript 提交中击败了19.77%的用户
  */
+
+/**
+ * 官方题解: 二分查找
+ */
+ var peakIndexInMountainArray = function(arr) {
+    const n = arr.length;
+    let left = 1, right = n - 2, ans = 0;
+
+    while (left <= right) {
+        const mid = Math.floor((left + right) /2 );
+        // 巧妙的跳过了中点和两边点的复杂判断~~
+        // 直接判断相邻元素就完全知道走向了嘛~~~~~
+        if (arr[mid] > arr[mid + 1]) {
+            ans = mid;
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+    }
+    return ans;
+};
