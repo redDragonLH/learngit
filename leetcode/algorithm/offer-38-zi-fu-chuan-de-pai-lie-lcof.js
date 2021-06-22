ebf691db214fd66bdb7e8fa2ca9e29cef932fc1c
LH <ok1332983774@163.com>
2021-06-22 23:04:04 +0800

    剑指 Offer 38. 字符串的排列 自题解 优化数据结构

--- leetcode/algorithm/offer-38-zi-fu-chuan-de-pai-lie-lcof.js
+++ leetcode/algorithm/offer-38-zi-fu-chuan-de-pai-lie-lcof.js
@@ -10,13 +10,20 @@
  * @return {string[]}
  */
 var permutation = function (s) {
-    const data = [];
+    const data = {};
+    const result = []
     deep(s, '', 0, data);
-    return data;
+    for (const key in data) {
+        if (Object.hasOwnProperty.call(data, key)) {
+            result.push(key);
+
+        }
+    }
+    return result;
 };
 const deep = (s, subString, pos, data) => {
     if (subString.length === s.length) {
-        !data.includes(subString) && data.push(subString);
+        !data[subString] && (data[subString] = true);
         return null;
     }
     for (let i = 0; i < s.length; i++) {
@@ -27,6 +34,6 @@ const deep = (s, subString, pos, data) => {
 }
 /**
  * 理论上不需要这么长时间的啊，难道是include 方法的问题，改为object么
- * 执行用时：3272 ms, 在所有 JavaScript 提交中击败了5.50%的用户
- * 内存消耗：45.7 MB, 在所有 JavaScript 提交中击败了93.69%的用户
+ * 执行用时：204  ms, 在所有 JavaScript 提交中击败了34.33%的用户
+ * 内存消耗：50.1 MB, 在所有 JavaScript 提交中击败了13.72%的用户
  */
